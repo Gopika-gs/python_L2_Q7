67c1aa020dbcdcda79670a79ec05fb68749b3847
Gopika-gs <gsgopika00@gmail.com>
2022-06-04 19:15:04 +0530

    files added

---
+++ L2_Q7.py
@@ -0,0 +1,7 @@
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+keys = dict_num.keys()
+print("keys in the dictionary : ",keys)
+value = dict_num.values()
+print("values in the dictionary : ",value)
+item = dict_num.items()
+print("items in the dictionary : ",item)
